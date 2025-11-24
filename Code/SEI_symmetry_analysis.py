64ea0cf04bb7e739a9b6ca1713ab55281e3f7d26
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2025-11-24 08:59:23 +0100

    New python script with symbolic calculations

---
+++ Code/SEI_symmetry_analysis.py
@@ -0,0 +1,15 @@
+# =========================================================
+# SEI symmetry analysis
+# Date: 2025-11-24,
+# Written by: Johannes
+# Description: We solve the linearised symmetry conditions for the SEI model in order to find the parameter symmetries.
+# =========================================================
+# LOAD LIBRARIES
+from sympy import * # For symbolic equations
+# =========================================================
+def main():
+    print("Hello!")
+
+# RUN THE MAIN FUNCTION
+if __name__ == "__main__":
+    main()   
