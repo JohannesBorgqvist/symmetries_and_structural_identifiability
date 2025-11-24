f805f2f5babcaaa478401393f8fa8a0b2bf28eb1
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2025-11-24 09:18:20 +0100

    Added output notes in a tex file

--- Code/SEI_symmetry_analysis.py
+++ Code/SEI_symmetry_analysis.py
@@ -8,7 +8,32 @@
 from sympy import * # For symbolic equations
 # =========================================================
 def main():
-    print("Hello!")
+    # Open a file in which we save all equations in LaTeX
+    f = open("./SEI_symmetry_analysis_document/notes.tex","a")
+    # Define all parameters as symbols\n",
+    c, beta, mu_S, mu_E, mu_I, delta, epsilon = symbols('c, beta, mu_S, mu_E, mu_I, delta, epsilon')
+    # Define our independent variable time
+    t = symbols('t')
+    # Define our three dependent variables being the states
+    S = Function('S')(t)
+    E = Function('E')(t)
+    I = Function('I')(t)
+    # Now, we define our three ODEs
+    # ODE for S
+    ODE_S = c - beta*S*I - mu_S*S
+    ODE_S = Eq(Derivative(S,t,1),ODE_S)
+    f.write("ODE for $S$:")
+    f.write(latex(ODE_S,mode='equation').replace("\\\\begin{equation}","\\\\begin{equation}\\n").replace("\\\\end{equation}","\\\\quad\\\\,,\\\\label{eq:ODE_S}\\n\\\\end{equation}\\n"))
+    # ODE for E
+    ODE_E = (1-epsilon)*beta*S*I - delta*E - mu_E*E
+    ODE_E = Eq(Derivative(E,t,1),ODE_E)
+    f.write("ODE for $E$:")
+    f.write(latex(ODE_E,mode='equation').replace("\\\\begin{equation}","\\\\begin{equation}\\n").replace("\\\\end{equation}","\\\\quad\\\\,,\\\\label{eq:ODE_E}\\n\\\\end{equation}\\n"))
+    # ODE for I
+    ODE_I = epsilon*beta*S*I + delta*E - mu_I*I
+    ODE_I = Eq(Derivative(I,t,1),ODE_I)
+    f.write("ODE for $I$:")
+    f.write(latex(ODE_I,mode='equation').replace("\\\\begin{equation}","\\\\begin{equation}\\n").replace("\\\\end{equation}","\\\\quad\\\\,.\\\\label{eq:ODE_I}\\n\\\\end{equation}\\n"))
 
 # RUN THE MAIN FUNCTION
 if __name__ == "__main__":
